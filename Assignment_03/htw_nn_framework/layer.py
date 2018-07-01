06452d009dad3498c54211a59120e37e1e4055b3
marisanest <marisanest@mailbox.org>
2018-07-01 15:22:34 +0200

    Implemented conv layer forward and backward computation

--- Assignment_03/htw_nn_framework/layer.py
+++ Assignment_03/htw_nn_framework/layer.py
@@ -53,45 +53,128 @@ class FullyConnected():
         return dX, [dW, db]
 
 class Conv():
-    ''' Description
+    '''         
+    Weights will be initilized with ... ?.
+    Biases will be initilized with zero.
     '''
     def __init__(self, X_dim, filter_num, filter_dim, stride, padding):
         self.X_dim = X_dim
         self.filter_num = filter_num
         self.filter_dim = filter_dim
         self.stride = stride
-        self.padding = padding
-        self.W = # TODO: init
-        self.b = # TODO: init
+        self.padding = int(((X_dim[2] - 1) * stride - X_dim[3] + filter_dim) / 2) if padding else 0
+        self.W = np.random.randn(filter_num, X_dim[1], filter_dim, filter_dim)
+        self.b = np.zeros((filter_num, 1, 1, 1))
         self.params = [self.W, self.b]
-        self.output_dim = self.output_dimension() # The dimensions of the Conv output.
 
     def forward(self, X):
         """
         """
-        # Initialize the output volume with zeros.
-        output = np.zeros(self.output_dim)
+        self.X = X
+        self.X_dim = self.X.shape
         
-        # Calculate image with padding and new image dimension.
-        image, self.image_dim = self.zero_padding(image)
+        # The dimensions of X.
+        (n, in_C, in_H, in_W) = self.X_dim
         
-        for x_i, x in tqdm(enumerate(X)):  # loop over image samples.
-            for h in range(self.output_dim[1]):  # loop over height indices of the output volume.
-                for w in range(self.output_dim[2]):  # loop over width indices of the output volume.
+        # The dimensions of output.
+        out_C = self.filter_num
+        out_H = int((in_H - self.filter_dim + 2 * self.padding) / self.stride) + 1
+        out_W = int((in_W - self.filter_dim + 2 * self.padding) / self.stride) + 1
+        
+        # Initialize output with the correct shapes and zeros.
+        output = np.zeros((n, out_C, out_H, out_W))
+        
+        # Add zero padding to X.
+        padded_X = self.zero_padding(X)
+        
+        for i in range(n):  # Loop over the batch of training samples.
+            
+            padded_x = padded_X[i] # Select ith training sample.
+          
+            for h in range(out_H):  # Loop over height indices of the output volume.
+                for w in range(out_W):  # Loop over width indices of the output volume.
+                    
+                    # Corner indices of the current training sample part.
+                    x_h_start = h * self.stride
+                    x_h_end = x_h_start + self.filter_dim
+                    x_w_start = w * self.stride
+                    x_w_end = x_w_start + self.filter_dim
+                    
+                    # Slice the current training sample part with respect to the corner indices.
+                    padded_x_slice = padded_x[:,x_h_start:x_h_end,x_w_start:x_w_end]
                 
-                    # Indices if the current image part that sould be used for computation.
-                    x_H_i_start = h * self.stride
-                    x_H_i_end = image_H_i_start + self.filter_dim
-                    x_W_i_start = w * self.stride
-                    x_W_i_end = image_W_i_start + self.filter_dim
-                
-                    for f_i, f in enumerate(self.W):
-                        output[x_i,h,w,f_i] = np.sum(x[x_H_i_start:x_H_i_end,x_W_i_start:x_W_i_end] * f)
+                    for c in range(out_C): # Loop over channel indices of the output volume.
+                        output[i,c,h,w] = np.sum(padded_x_slice * self.W[c,:,:,:] + self.b[c,:,:,:])
                     
         return output
 
     def backward(self, dout):
-        return None
+        """Implement the backward propagation for a convolution layer.
+    
+        Args:
+            dout (ndarray): Gradient with respect to the output of the conv layer
+        Returns:
+            dX (ndarray): Gradient with respect to the input of the conv layer (X)
+            dW (ndarray): Gradient with respect to the weights of the conv layer (W)
+            db (ndarray): Gradient with respect to the biases of the conv layer (b)
+        """
+        
+        # The dimensions of X.
+        (n, in_C, in_H, in_W) = self.X_dim
+        
+        # The dimensions of dout.
+        (n, out_C, out_H, out_W) = dout.shape
+          
+        # Initialize dX, dW, db with the correct shapes and zeros.
+        dX = np.zeros((n, in_C, in_H, in_W))                           
+        dW = np.zeros((out_C, in_C, self.filter_dim, self.filter_dim))
+        db = np.zeros((out_C, 1, 1, 1))
+
+        # Add zero padding to X and dX.
+        padded_X =  self.zero_padding(self.X)
+        padded_dX = self.zero_padding(dX)
+        
+        for i in range(n):                       # Loop over the batch of training samples.
+        
+            # Select ith training sample from padded_X and padded_dX.
+            padded_x = padded_X[i]
+            padded_dx = padded_dX[i]
+        
+            for h in range(out_H):                   # Loop over height indices of the output volume.
+                for w in range(out_W):               # Loop over width indices of the output volume.
+                    
+                    # Corner indices of the current training sample part.
+                    x_h_start = h * self.stride
+                    x_h_end = x_h_start + self.filter_dim
+                    x_w_start = w * self.stride
+                    x_w_end = x_w_start + self.filter_dim
+                    
+                    # Slice the current training sample part with respect to the corner indices.
+                    padded_x_slice = padded_x[:,x_h_start:x_h_end,x_w_start:x_w_end]
+                        
+                    for c in range(out_C):           # Loop over the channels of the output volume.
+                        # Update gradients.
+                        padded_dx[:,x_h_start:x_h_end,x_w_start:x_w_end] += self.W[c,:,:,:] * dout[i, c, h, w]
+                        dW[c,:,:,:] += padded_x_slice * dout[i, c, h, w]
+                        db[c,:,:,:] += dout[i, c, h, w]
+                    
+            # Set the ith training example's dX to the unpaded padded_dx
+            dX[i, :, :, :] = padded_dx[:,self.padding:-self.padding,self.padding:-self.padding]
+    
+        # Making sure output shape is correct
+        assert(dX.shape == (n, in_C, in_H, in_W))
+    
+        return dX, [dW, db]
+    
+    def zero_padding(self, X):
+        """ Add zero padding to all images in X.
+    
+        Args:
+            X (ndarray): Batch of images.
+        Returns:
+            Padded images.
+        """
+        return np.pad(X,((0,0),(0,0),(self.padding,self.padding),(self.padding,self.padding)),'constant',constant_values = 0)
 
 
 class Pool():
@@ -102,32 +185,104 @@ class Pool():
         self.func = func
         self.filter_dim = filter_dim
         self.stride = stride
-        self.W = # TODO: init
-        self.b = # TODO: init
+        #self.W = # TODO: init
+        #self.b = # TODO: init
         self.params = [self.W, self.b]
         self.output_dim = self.output_dimension() # The dimensions of the Conv output.
         
     def forward(self, X):
-        # Initialize the output volume with zeros.
-        output = np.zeros(self.output_dim)
+        """
+        """
+        self.X = X
+        self.X_dim = self.X.shape
         
-        for x_i, x in tqdm(enumerate(X)):  # loop over image samples.
-            for h in range(self.output_dim[1]):  # loop over height indices of the output volume.
-                for w in range(self.output_dim[2]):  # loop over width indices of the output volume.
+        # The dimensions of X.
+        (n, in_H, in_W, in_C) = self.X_dim
+        
+        # The dimensions of output.
+        out_H = int((in_H - self.filter_dim + 2 * self.padding) / self.stride) + 1
+        out_W = int((in_W - self.filter_dim + 2 * self.padding) / self.stride) + 1
+        out_C = self.filter_num
+        
+        # Initialize output with the correct shapes and zeros.
+        output = np.zeros((n, out_H, out_W, out_C))
+        
+        for i in range(n):  # Loop over the batch of training samples.
+            
+            x = X[i] # Select ith training sample.
+            
+            for h in range(out_H):  # Loop over height indices of the output volume.
+                for w in range(out_W):  # Loop over width indices of the output volume.
                 
-                    # Indices if the current image part that sould be used for computation.
-                    x_H_i_start = h * self.stride
-                    x_H_i_end = image_H_i_start + self.filter_dim
-                    x_W_i_start = w * self.stride
-                    x_W_i_end = image_W_i_start + self.filter_dim
+                    # Corner indices of the current training sample part.
+                    x_h_start = h * self.stride
+                    x_h_end = x_h_start + self.filter_dim
+                    x_w_start = w * self.stride
+                    x_w_end = x_w_start + self.filter_dim
                     
-                    for f_i, f in enumerate(self.W):
-                        output[x_i,h,w,f_i] = self.pooling_function(x[x_H_i_start:x_H_i_end,x_W_i_start:x_W_i_end,:])
-                    
-        return output
+                    # Slice the current training sample part with respect to the corner indices.
+                    x_slice = padded_x[x_h_start:x_h_end,x_w_start:x_w_end,:]
 
+                    for c in range(out_C): # Loop over channel indices of the output volume.
+                        output[i,h,w,c] = self.pooling_function(x_slice)
+        return output
+        
     def backward(self, dout):
-        return None
+        """Implement the backward propagation for a pooling layer.
+    
+        Args:
+            dout (ndarray): Gradient with respect to the output of the pool layer
+        Returns:
+            dX (ndarray): Gradient with respect to the input of the pool layer (X)
+        """
+        # The dimensions of X.
+        (n, in_H, in_W, in_C) = self.X_dim
+        
+        # The dimensions of dout.
+        (n, out_H, out_W, out_C) = dout.shape
+          
+        # Initialize dX with the correct shapes and zeros.
+        dX = np.zeros(self.X_dim)
+    
+        for i in range(n):                       # Loop over the batch of training samples.
+        
+            x = self.X[i] # Select ith training sample.
+        
+            for h in range(out_H):                   # Loop over height indices of the output volume.
+                for w in range(out_W):               # Loop over width indices of the output volume.
+                    
+                    # Corner indices of the current training sample part.
+                    x_h_start = h * self.stride
+                    x_h_end = x_h_start + self.filter_dim
+                    x_w_start = w * self.stride
+                    x_w_end = x_w_start + self.filter_dim
+                    
+                    for c in range(out_C):           # Loop over channel indices of the output volume.
+                        # Compute the backward propagation in both modes.
+                        if mode == "max":
+                        
+                            # Use the corners and "c" to define the current slice from a_prev (≈1 line)
+                            x_slice = a_prev[x_h_start:x_h_end,x_w_start:x_w_end,c]
+                            # Create the mask from a_prev_slice (≈1 line)
+                            mask = create_mask_from_window(a_prev_slice)
+                            # Set dA_prev to be dA_prev + (the mask multiplied by the correct entry of dA) (≈1 line)
+                            dX[i,x_h_start:x_h_end,x_w_start:x_w_end,c] += (mask * x_slice)
+                        
+                        elif mode == "average":
+                        
+                            # Get the value a from dA (≈1 line)
+                            dx = dX
+                            # Define the shape of the filter as fxf (≈1 line)
+                            shape = (self.filter_dim,self.filter_dim)
+                            # Distribute it to get the correct slice of dA_prev. i.e. Add the distributed value of da. (≈1 line)
+                            dX[i,x_h_start:x_h_end,x_w_start:x_w_end,c] += dx
+                        
+
+        # Making sure your output shape is correct
+        assert(dX.shape == self.X.shape)
+    
+        return dX
+
 
 class Batchnorm():
     ''' Description
